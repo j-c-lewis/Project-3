1ad625488eff1273138a26d9ad37ee604fe911fd
Ja_Lew <dladyj@gmail.com>
2019-06-04 19:44:41 -0400

    Added the Label for Other Input Field

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+function replaceContentInContainer(target, source) {
+    document.getElementById(target).innerHTML = document.getElementById(source).innerHTML;
+  }
+  
+  function replaceContentInOtherContainer(replace_target, source) {
+    document.getElementById(replace_target).innerHTML = document.getElementById(source).innerHTML;
+  }
