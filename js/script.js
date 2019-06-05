ce46f703b759fc103820ebeff10ae69dd97683cb
Ja_Lew <dladyj@gmail.com>
2019-06-04 20:09:08 -0400

    Commented Previous Show/Hide function
    commented show/ hide function, added new show/ hide function

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 
 
 
-function replaceContentInContainer(target, source) {
+/** 
+ * function replaceContentInContainer(target, source) {
     document.getElementById(target).innerHTML = document.getElementById(source).innerHTML;
   }
   
   function replaceContentInOtherContainer(replace_target, source) {
     document.getElementById(replace_target).innerHTML = document.getElementById(source).innerHTML;
   }
+*/
+
+
+  $(document).ready(
+    function()
+    {
+      $('').removeClass('');
+    }
+ );
