58cea667dc4880d4e6463aa58a5d9afda8ba34b8
Ja_Lew <dladyj@gmail.com>
2019-06-05 12:56:06 -0400

    Added show Hide function
    added show/ hide for loop for design

--- js/script.js
+++ js/script.js
@@ -15,10 +15,41 @@
 /** 
  * Targets "other" function, hides it so it displays even if javascript is disabled 
  * but is hidden at first*/
-  $(document).ready(
+ 
+$(document).ready(
     function()
     {
       $('').removeClass('');
     }
  );
- 
+
+/** 
+ *  function change(colorOp) {
+
+
+  let selectBox = colorOp;
+  let selected = selectBox.options[selectBox.selectedIndex].value;
+  let textarea = document.getElementById("design");
+
+  if(selected === 'Theme - JS Puns'){
+      textarea.style.display = ("Coenflower Blue", "Dark Slate Grey", and "Gold");
+  }
+  else{
+      textarea.style.display = "I &#9829; JS shirt only";
+      textarea.style.display =("Tomato," "Steel Blue," and "Dim Grey.")
+  }
+}
+*/
+
+ /*** 
+  * onchange="change()";
+  * $('#show').on('change', function () {
+   var optionSelected = $("option:selected", this);
+   var valueSelected = this.value;
+   if(valueSelected == 1){
+       $("#text_area").show();
+   } else {
+       $("#text_area").hide();
+   }
+});
+ */
